9acd1c113bd3d5d6d977dfaa66530bd4b215361c
Axel Fornerod <104044533+Axelforn@users.noreply.github.com>
2023-05-10 00:01:26 -0300

    Ejercicios de enumeraciones

--- Java/Semana02/Enumeraciones/src/enumeraciones/Dias.java
+++ Java/Semana02/Enumeraciones/src/enumeraciones/Dias.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package enumeraciones;
 
-/**
- *
- * @author USUARIO
- */
-public class Dias {
-    
+public enum Dias {
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO 
 }
