2a708fac013e1a16960b4e7e16ae6159ad1a76e0
Axel Fornerod <fornerodaxel@hotmail.com>
2023-05-29 13:31:56 -0300

    Leccion 04 completa

---
+++ Python/Semana04/Leccion04/prueba_db.py
@@ -0,0 +1,19 @@
+import psycopg2 #Para conectarnos a Postgre
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia) #De esta manera ejecutamos la sentencia
+registros = cursor.fetchall() #Te permite recuperar todos los registros de la sentencia
+print(registros)
+
+cursor.close()
+conexion.close()
+
