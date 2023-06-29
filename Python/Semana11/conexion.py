41c11a2a486fd8135d0cea4d5ae07cd1785639ac
Axel Fornerod <fornerodaxel@hotmail.com>
2023-06-29 19:11:55 -0300

    Agregamos liberarConexion

--- Python/Semana11/conexion.py
+++ Python/Semana11/conexion.py
@@ -39,6 +39,13 @@ class Conexion:
         else:
             return cls._pool
 
+    @classmethod
+    def liberarConexion (cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f'Regresamos la conexion del pool')
+
+        
+
 if __name__ == '__main__':
     conexion1 = Conexion.obtenerConexion()
     conexion2 = Conexion.obtenerConexion()
